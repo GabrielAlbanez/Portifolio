d8090dc10e9379dd25258b6ea30b1a1e32b8e5bf
GabrielAlbanez <gabriel.g.abanez@gmail.com>
2023-09-04 12:18:14 -0300

    reponsive v3

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -10,14 +10,14 @@ const Home = () => {
         <section id='home' className='flex flex-col w-[100%] border-b-[1px] h-[100vh] '>
             <div className='flex items-center justify-center xl:justify-around h-[130%] pt-10 '>
                 <div className='h-[50vh] w-[60vh] flex flex-col justify-center '>
-                    <div className='flex flex-col items-center xl:items-start bg-slate-300 w-[100%]  gap-10 xl:p-10 md:h-[90%] h-[80%]'>
+                    <div className='flex flex-col items-center xl:items-start  w-[100%]  gap-10 xl:p-10 md:h-[90%] h-[80%]'>
                         <h2 className='text-2xl'>OLA, EU SOU:</h2>
-                        <h1 className='text-3xl sm:text-6xl  sm:pt-3 xl: writeText '>Gabriel Albanez</h1>
-                        <hr className='bg-violet-300 h-1 w-[70%] xl:w-[100%]  my-2' />
+                        <h1 className='text-4xl sm:text-6xl  sm:pt-3 writeText2 '>Gabriel Albanez</h1>
+                        <hr className='bg-violet-300 h-1 w-[70%]  lg:w-[100%]  my-2' />
                         <h2 className='text-2xl'>DESENVOLVEDOR FRONT-END</h2>
 
                     </div>
-                    <div className='flex gap-10 items-center justify-center sm:justify-start p-10'>
+                    <div className='flex gap-10 items-center justify-center xl:justify-start pt-24 lg:pt-16 xl:pt-6 p-10'>
                         <button className='flex gap-2 items-center text-md rounded-full border-black border-[2px] px-5 py-2 shadow-violet-300 transition shadow-xl hover:bg-zinc-900 hover:text-white animate-bounce-short'>Linkedin <AiFillLinkedin size={25} /></button>
                         <button className='flex gap-2 items-center text-md rounded-full border-black border-[2px] px-5 py-2 shadow-violet-300 transition shadow-xl hover:bg-zinc-900 hover:text-white animate-bounce-short'>GitHub <AiOutlineGithub size={25} /></button>
                     </div>
