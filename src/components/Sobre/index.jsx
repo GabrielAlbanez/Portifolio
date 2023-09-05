723b6eb4e300d8d33c966dd3dff4b70d21c4d079
GABRIEL GONCALVES ALBANEZ <gabriel.galbanez@senacsp.edu.br>
2023-09-05 17:15:40 -0300

    theve version 2.0

--- src/components/Sobre/index.jsx
+++ src/components/Sobre/index.jsx
@@ -12,7 +12,7 @@ const Sobre = () => {
 
   const colorExperience = darkModeActive
   ? 'bg-zinc-900 text-white '
-  : 'bg-white text-black border-b-[2px]';
+  : 'bg-white text-black ';
 
   const bordExperience = darkModeActive
   ? 'rounded-lg shadow-md shadow-violet-300 '
@@ -24,7 +24,7 @@ const Sobre = () => {
 
 
   return (
-    <section data-aos='fade-right' data-aos-duration='1000'data-aos-delay='300' id='sobre' className={`flex   w-screen justify-center gap-24   items-center  h-[90vh] ${colorExperience}`}>
+    <section data-aos='fade-right' data-aos-duration='1000'data-aos-delay='300'  data-aos-once='true' id='sobre' className={`flex   w-screen justify-center gap-24   items-center  h-[90vh] ${colorExperience}`}>
       <div className={`${bordExperience} flex items-center justify-between px-48 w-[95%] h-[90%] `}>
         <div className=''>
           <img src={Person} alt="" className='object-cover w-[60vh] h-[100%] rounded-lg  shadow-violet-300 shadow-xl' />
@@ -34,7 +34,7 @@ const Sobre = () => {
           <h2 className='text-2xl'>Campinas, São Paulo</h2>
           <p className='text-xl'>Ola, autalmente estou cursando 2 cursos de ti, um providenciado pela Fiec e o outro curso na instituição Senac,pretendo me tornar futuramente um desenvolvedor Front-End,em meus 4 primeiros meses de vida ja consegui autar na area como freelancer com alguns projetos que serão apresentados nesse portifolio</p>
 
-          <button className=' flex mt-7 items-center gap-1 justify-center w-36 px-5 py-2 rounded-full border-black border-[2px] shadow-violet-300 transition shadow-xl hover:bg-zinc-900 hover:text-white animate-bounce-short '>Curriculo <AiOutlineCloudDownload size={30}/></button>
+          <button className={` flex mt-7 items-center gap-1 justify-center w-36 px-5 py-2 rounded-full border-black border-[2px] shadow-violet-300 transition shadow-xl ${colorButtonHome}`}>Curriculo <AiOutlineCloudDownload size={30}/></button>
         </div>
         <div>
 
